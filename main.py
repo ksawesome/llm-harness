e3f0b9d72012c44915472d33c6a93b3d15bb8488
Kshitish Kiran Madbhavi <kshitish.madbhavi@gmail.com>
2025-10-13 03:44:22 +0530

    Reformat main.py with Black

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ async def run_benchmark(args):
             prompt_text = prompt_data["prompt_text"]
 
             if prompt_data.get("teacher_context"):
-                context = prompt_data['teacher_context']
+                context = prompt_data["teacher_context"]
                 full_prompt = f"Context: {context}\n\n{prompt_text}"
             else:
                 full_prompt = prompt_text
